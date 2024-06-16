6e256689a4b55e6314f85b8c843db2aad929bf32
ozgurd5 <ozgurdalbeler@gmail.com>
2024-06-16 22:07:36 +0300

    hot post support, random seed and better debug messages

--- program_settings.py
+++ program_settings.py
@@ -1,14 +1,22 @@
-﻿# Do not change the keys, only the values
-program_settings = {
-    "has_debug": True,
+﻿# Do not change the keys (left side of the colon), only change the values (right side of the colon)
 
-    # Reddit settings
+reddit_settings = {
+    # Paths
+    "subreddits_file_path": "subreddits.txt",  # Path to the file that contains the subreddits, each line should be a subreddit name, e.g. "AskReddit"
+    "post_list_file_path": "post_list.txt",  # Path to the file that contains the posts that have been selected before
+
+    # Parameters
+    "is_top_post": True,  # If True, selects a post from the top posts of the subreddit, if False, selects a post from the hot posts of the subreddit
     "post_limit": 50,  # Should not be smaller than 1
     "time_filter": "all",  # Can be one of "all", "day", "hour", "month", "week", "year"
 
     # Example
+    # "is_top_post": True,
     # "post_limit": 50,
     # "time_filter": "all",
     # Selects a random subreddit from the subreddits.txt, then selects a random post from the top 50 posts of the subreddit in all time.
 }
 
+program_settings = {
+    "has_debug": True,  # If True, prints debug messages
+}
--- reddit.py
+++ reddit.py
@@ -1,26 +1,38 @@
-from utilities import debug_print, program_settings
+from utilities import debug_print
+from program_settings import reddit_settings
 from dotenv import dotenv_values
 import praw
 import random
+from datetime import datetime
 
 
 def get_random_text_from_reddit():
+    init_random_seed()
     reddit = init_reddit()
 
     subreddits_to_check = get_subreddits()
     subreddit_amount = len(subreddits_to_check)
+
+    top_or_hot = "top" if reddit_settings["is_top_post"] else "hot"
+    time_filter_or_none = f" sorted by '{reddit_settings["time_filter"]}'" if reddit_settings["is_top_post"] else ""
 
     while True:  # Loop to find a subreddit where not all posts have been selected
         subreddit_name = random.choice(subreddits_to_check)
         subreddit = reddit.subreddit(subreddit_name)
 
         post_find_flag = False
-        indexes_to_select = list(range(1, program_settings["post_limit"] + 1))
+        indexes_to_select = list(range(1, reddit_settings["post_limit"] + 1))
 
         while True:  # Loop to find a post that is not selected before
             random_post_index = random.choice(indexes_to_select)
-            top_posts = list(subreddit.top(time_filter=program_settings["time_filter"], limit=random_post_index))
-            selected_post = top_posts[random_post_index - 1]  # Indexing starts from 0 in list but 1 in Reddit
+
+            if reddit_settings["is_top_post"]:
+                selected_posts = list(subreddit.top(time_filter=reddit_settings["time_filter"], limit=random_post_index))
+
+            else:
+                selected_posts = list(subreddit.hot(limit=random_post_index))
+
+            selected_post = selected_posts[random_post_index - 1]  # Indexing starts from 0 in list but 1 in Reddit
 
             if not check_post_in_list(selected_post.url):
                 post_find_flag = True
@@ -30,8 +42,8 @@ def get_random_text_from_reddit():
                 indexes_to_select.remove(random_post_index)
 
             if len(indexes_to_select) == 0:
-                debug_print(f"All posts have been seen by given situation: r/{subreddit_name}, top {program_settings['post_limit']} posts in "
-                            f"{program_settings['time_filter']}")
+                debug_print(f"All posts have been seen by given situation: r/{subreddit_name}, {top_or_hot} {reddit_settings['post_limit']} posts"
+                            f"{time_filter_or_none}")
                 break
 
         if post_find_flag:
@@ -39,13 +51,13 @@ def get_random_text_from_reddit():
 
         subreddits_to_check.remove(subreddit_name)
         if len(subreddits_to_check) == 0:
-            debug_print(f"All posts have been seen in all subreddits by given situation: top {program_settings['post_limit']} posts in "
-                        f"{program_settings['time_filter']} from {subreddit_amount} subreddits")
+            debug_print(f"All posts have been seen in all subreddits by given situation: {top_or_hot} {reddit_settings['post_limit']} posts"
+                        f"{time_filter_or_none} from {subreddit_amount} subreddits")
             return
 
     add_post_to_list(selected_post.url)
 
-    debug_print(f"Fetching {random_post_index}th post from r/{subreddit_name}")
+    debug_print(f"Fetching {random_post_index}th {top_or_hot} post{time_filter_or_none} from r/{subreddit_name}")
     return selected_post.selftext
 
 
@@ -67,18 +79,25 @@ def init_reddit():
     return reddit
 
 
+def init_random_seed():
+    now = datetime.now()
+    seed = int(f"{now.year}{now.month:02d}{now.day:02d}{now.hour:02d}{now.minute:02d}{now.second:02d}{now.microsecond:06}")
+    debug_print("setting seed: ", seed)
+    random.seed(seed)
+
+
 def get_subreddits():
-    with open("subreddits.txt", 'r', encoding="utf-8-sig") as subreddits:
+    with open(reddit_settings["subreddits_file_path"], 'r', encoding="utf-8-sig") as subreddits:
         return [line.strip() for line in subreddits.readlines()]
 
 
 def add_post_to_list(selected_post):
-    with open("post_list.txt", 'a', encoding="utf-8-sig") as post_list:
+    with open(reddit_settings["post_list_file_path"], 'a', encoding="utf-8-sig") as post_list:
         post_list.write(selected_post + "\n")
 
 
 def check_post_in_list(selected_post):
-    with open("post_list.txt", 'r', encoding="utf-8-sig") as post_list:
+    with open(reddit_settings["post_list_file_path"], 'r', encoding="utf-8-sig") as post_list:
         post_list = post_list.readlines()
         for post in post_list:
             if post == selected_post + "\n":
