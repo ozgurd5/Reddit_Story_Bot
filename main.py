cba5d0f7632431909853d594af9459af95221b32
ozgurd5 <ozgurdalbeler@gmail.com>
2024-06-16 22:49:00 +0300

    movie py fundamentals

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 ﻿from utilities import debug_print
 from reddit import get_random_text_from_reddit
+import moviepy.editor as moviepy
 
 
 def main():
-    debug_print(get_random_text_from_reddit())
+    text = get_random_text_from_reddit()
+    debug_print(text)
+
+    clip = moviepy.VideoFileClip("gameplay_footage/subway_surfers.mp4")
+    subclip = clip.subclip(20, 50)
+
+    txt_clip = moviepy.TextClip(text, fontsize=20, color='white')
+    txt_clip = txt_clip.set_position(("center", "center")).set_duration(10)
+
+    final_clip = moviepy.CompositeVideoClip([subclip, txt_clip])
+    final_clip.write_videofile("output.mp4")
 
 
 # Call the main function
